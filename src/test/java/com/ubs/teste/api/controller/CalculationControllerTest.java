018046c9dc28cd2c3e2431b28d58c269f309781d
lincolntec <lincoln_tec@hotmail.com>
2021-02-23 08:15:31 -0300

    Remoção de não utilizavel

--- src/test/java/com/ubs/teste/api/controller/CalculationControllerTest.java
+++ src/test/java/com/ubs/teste/api/controller/CalculationControllerTest.java
@@ -7,8 +7,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.ubs.teste.api.dto.ProductDTO;
-
 import junit.framework.Assert;
 
 class CalculationControllerTest {
@@ -30,51 +28,5 @@ class CalculationControllerTest {
         } catch (Exception e) {
 
         }
-    }
-
-
-    @Test
-    void calculation() {
-
-        ProductDTO prod = new ProductDTO();
-        prod.setProduct("EMMS");
-        prod.setStoryQuantity(5);
-
-        int qtdProduct = 11;
-
-        int media = qtdProduct / prod.getStoryQuantity();
-
-        System.out.println("Media:" + media);
-
-        // int resultado = prod.getStoryQuantity() % media;
-        int resultado = qtdProduct % prod.getStoryQuantity();
-
-        System.out.println("Resultado do mod: " + resultado);
-
-        System.out.println("Existem " + prod.getStoryQuantity() + " empresas cada uma ficará com " + media
-                + " produtos e o restante ficará com " + resultado);
-
-        int sobra = prod.getStoryQuantity() - resultado;
-
-        if (media >= 2) {
-            media = media + 1;
-            System.out.println("Existem " + prod.getStoryQuantity() + " empresas " + (media + 1)
-                    + " empresas ficaram com " + media + " produtos e o restante ficará com " + sobra);
-
-            int teste = ((media + 1) * media);
-
-            int teste2 = qtdProduct - teste;
-
-            int teste3 = prod.getStoryQuantity() - resultado;
-
-            System.out.println("Existem " + prod.getStoryQuantity() + " empresas " + (media + 1)
-                    + " empresas ficaram com " + media + " produtos e o restante ficará com " + teste2);
-
-            System.out.println(
-                    "Existem " + prod.getStoryQuantity() + " empresas " + (media + 1) + " empresas ficaram com " + media
-                            + " produtos e " + teste3 + " empresa ficará com " + (media - 1));
-
-        }
-
     }
 }
